14785486b43dd16b4055d7bc8a5f3ddb829798ba
Wizzerhawk <njyoung@disroot.org>
2025-06-06 15:49:43 +1200

    Header wording change

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -85,7 +85,7 @@ export default function Header() {
     <header className={styles.header}>
       <div className={styles.container}>
         <Link href="/" className={styles.logo}>
-          Language App
+          Nathan's Language App
         </Link>
         
         <nav className={styles.nav}>
